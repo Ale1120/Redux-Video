fa97c40aaded48224ae2f36a55fc9a66b4c5fd94
Ale1120 <alejandro.ortegano11@gmail.com>
2018-12-14 12:22:02 -0400

    add store on app Video class-15

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home.js';
 import data from '../api.json';
+import { createStore } from 'redux';
 //import Playlist from './src/playlist/components/playlist.js';
 //import data from './src/api.json';
+
+const initialState = {
+  data: {
+    ...data
+  }
+}
+
+const store = createStore (
+  (state) => state,
+  initialState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+console.log(store.getState());
+
 const homeContainer = document.getElementById('home-container')
 // import ReactDom from 'react-dom';
 //ReactDom.render(que voy a rendrizar, donde lo hare);
